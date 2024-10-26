26bd299e9ec93e0e50ea3d2357b549ea41e10d36
Julian <jdymail99@gmail.com>
2024-10-26 16:26:03 +0200

    rewrite/refactor particle class

--- src/Particle.h
+++ src/Particle.h
@@ -10,75 +10,23 @@
 #include <array>
 #include <string>
 
-class Particle {
-private:
-	/**
-	 * Position of the particle
-	 */
-	std::array<double, 3> x;
 
-	/**
-	 * Velocity of the particle
-	 */
-	std::array<double, 3> v;
-
-	/**
-	 * Force effective on this particle
-	 */
-	std::array<double, 3> f;
-
-	/**
-	 * Force which was effective on this particle
-	 */
-	std::array<double, 3> old_f;
-
-	/**
-	 * Mass of this particle
-	 */
-	double m;
-
-	/**
-	 * Type of the particle. Use it for whatever you want (e.g. to separate
-	 * molecules belonging to different bodies, matters, and so on)
-	 */
-	int type;
-
-public:
+using vec3 = std::array<double, 3>;
+struct Particle {
 	explicit Particle(int type = 0);
-
+	Particle(vec3 position, vec3 velocity, double mass, int type = 0);
 	Particle(const Particle& other);
+	~Particle();
 
-	Particle(
-		// for visualization, we need always 3 coordinates
-		// -> in case of 2d, we use only the first and the second
-		std::array<double, 3> x_arg, std::array<double, 3> v_arg, double m_arg,
-		int type = 0);
+	[[nodiscard]] std::string to_string() const;
+	bool operator==(const Particle& other) const;
 
-	virtual ~Particle();
-
-	const std::array<double, 3>& getX() const;
-	void setX(std::array<double, 3>& x);
-
-	const std::array<double, 3>& getV() const;
-	void setV(std::array<double, 3>& x);
-
-
-	const std::array<double, 3>& getF() const;
-	void setF(std::array<double, 3>& x);
-
-
-	const std::array<double, 3>& getOldF() const;
-	void setOldF(std::array<double, 3>& x);
-
-
-	double getM() const;
-
-	int getType() const;
-
-
-	bool operator==(Particle& other);
-
-	std::string toString() const;
+	vec3 position;
+	vec3 velocity;
+	vec3 force;
+	vec3 old_force;
+	double mass;
+	int type;
 };
 
 std::ostream& operator<<(std::ostream& stream, Particle& p);
