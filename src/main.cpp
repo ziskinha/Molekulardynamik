4ed41582b5f123c57aefd52770e6decb09530379
Julian <jdymail99@gmail.com>
2024-11-09 13:49:02 +0100

    fix minor bug in main

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ int main(const int argc, char* argv[]) {
     particles.add_cuboid({0,0,0}, {0,0,0}, {40, 8, 1}, 0.1, 1.1225, 1, 2, 0);
     particles.add_cuboid({15,15,0}, {0,-10,0}, {8, 8, 1}, 0.1, 1.1225, 1, 2, 1);
 
-    auto writer = args.benchmark ? nullptr : createWriter(args.output_format, args.delete_output_folder_contents);
+    auto writer = args.benchmark ? nullptr : createWriter(args.output_format, args.override);
     md::Integrator::StoermerVerlet simulator(particles, md::force::lennard_jones(5, 1), std::move(writer));
     simulator.simulate(0, args.duration, args.dt, write_freq, args.benchmark);
 
