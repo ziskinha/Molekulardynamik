5b7cf3abf72f9500d4a8bf96be570f7a7c66ee83
stagrz04 <stanislaw.grzegorski@tum.de>
2024-12-02 18:41:24 +0100

    edited verlet test and xml file

--- testing/StoermerVerletTests.cpp
+++ testing/StoermerVerletTests.cpp
@@ -10,9 +10,10 @@ void build_env(md::env::Environment& env) {
     boundary.origin = {0, 0, 0};
     boundary.set_boundary_rule(md::env::BoundaryRule::OUTFLOW);
     env.set_boundary(boundary);
-    env.add_particle({1, 5, 4}, {3, 3, 3}, 5, 0);
+    env.add_particle({1, 5, 0}, {3, 3, 3}, 5, 0);
     env.add_particle({3, 2, 1}, {0, 0, 0}, 5, 0);
     env.set_grid_constant(1);
+    env.set_force(md::env::InverseSquare(1,10));
     env.build();
 }
 
@@ -28,27 +29,28 @@ TEST(StoermerVerletTest, test){
     const auto& afterSimParticle1 = env.operator[](0);
     const auto& afterSimParticle2 = env.operator[](1);
 
+    ASSERT_TRUE(afterSimParticle1.position[0]==4);
     ASSERT_TRUE(afterSimParticle1.position[1]==8);
-    ASSERT_TRUE(afterSimParticle1.position[2]==7);
+    ASSERT_TRUE(afterSimParticle1.position[2]==3);
 
     ASSERT_TRUE(afterSimParticle2.position[0]==3);
     ASSERT_TRUE(afterSimParticle2.position[1]==2);
     ASSERT_TRUE(afterSimParticle2.position[2]==1);
 
-    //EXPECT_NEAR(env.operator[](0).force.operator[](0),-0.0400825846562090972409065881321059600046652999133668710917088161, 1e-5);
-    //EXPECT_NEAR(env.operator[](0).force.operator[](1), -0.2404955079372545834454395287926357600279917994802012265502528966, 1e-5);
-    //EXPECT_NEAR(env.operator[](0).force.operator[](2), -0.2404955079372545834454395287926357600279917994802012265502528966, 1e-5);
+    EXPECT_NEAR(env.operator[](0).force.operator[](0),0, 1e-5);
+    EXPECT_NEAR(env.operator[](0).force.operator[](1), 0, 1e-5);
+    EXPECT_NEAR(env.operator[](0).force.operator[](2), 0, 1e-5);
 
-    EXPECT_NEAR(afterSimParticle2.velocity[0],3-0.00400825846562090972409065881321059600046652999133668710917088161,1e-5);
-    EXPECT_NEAR(afterSimParticle2.velocity[1],3-0.02404955079372545834454395287926357600279917994802012265502528966,1e-5);
-    EXPECT_NEAR(afterSimParticle2.velocity[2],3-0.02404955079372545834454395287926357600279917994802012265502528966,1e-5);
+    EXPECT_NEAR(afterSimParticle1.velocity[0],3,1e-5);
+    EXPECT_NEAR(afterSimParticle1.velocity[1],3,1e-5);
+    EXPECT_NEAR(afterSimParticle1.velocity[2],3,1e-5);
+    
+    EXPECT_NEAR(env.operator[](1).force.operator[](0), 0, 1e-5);
+    EXPECT_NEAR(env.operator[](1).force.operator[](1), 0, 1e-5);
+    EXPECT_NEAR(env.operator[](1).force.operator[](2), 0, 1e-5);
 
-    //EXPECT_NEAR(env.operator[](1).force.operator[](0), 0.0400825846562090972409065881321059600046652999133668710917088161, 1e-5);
-    //EXPECT_NEAR(env.operator[](1).force.operator[](1), 0.2404955079372545834454395287926357600279917994802012265502528966, 1e-5);
-    //EXPECT_NEAR(env.operator[](1).force.operator[](2), 0.2404955079372545834454395287926357600279917994802012265502528966, 1e-5);
-
-    EXPECT_NEAR(afterSimParticle1.velocity[0],0.00400825846562090972409065881321059600046652999133668710917088161,1e-5);
-    EXPECT_NEAR(afterSimParticle1.velocity[1],0.02404955079372545834454395287926357600279917994802012265502528966,1e-5);
-    EXPECT_NEAR(afterSimParticle1.velocity[2],0.02404955079372545834454395287926357600279917994802012265502528966,1e-5);
+    EXPECT_NEAR(afterSimParticle2.velocity[0],0,1e-5);
+    EXPECT_NEAR(afterSimParticle2.velocity[1],0,1e-5);
+    EXPECT_NEAR(afterSimParticle2.velocity[2],0,1e-5);
 }
 
